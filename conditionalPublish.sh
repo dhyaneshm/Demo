947d3ee44f49c7ab89fbc7ec372aafb8c8e27127
Dhyan <dhyan@dhyans.com>
2016-11-13 17:18:58 -0500

    fix

--- conditionalPublish.sh
+++ conditionalPublish.sh
@@ -4,7 +4,7 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo '$TRAVIS_PULL_REQUEST is false, running tests and publishing'
     #./gradlew clean test bintrayUpload -x distTar -x distZip -Pbuild=${TRAVIS_BUILD_NUMBER} -Prelease=1.0
-    ./gradlew clean test distZip -Pbuild=${TRAVIS_BUILD_NUMBER} -Prelease=1.0
+    ./gradlew clean distZip -Pbuild=${TRAVIS_BUILD_NUMBER} -Prelease=1.0
 
 else
     echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running just tests and skipping publish'
